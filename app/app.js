34ef8f1a2dcf5b2fe3ee677503236a83a43f56c5
Sven van de Scheur <svenvandescheur@gmail.com>
2025-11-15 20:00:08 +0100

    :recycle: revert tile layer for now

--- app/app.js
+++ app/app.js
@@ -212,12 +212,10 @@ function initMap() {
   L.control.zoom({
     position: 'bottomright'
 }).addTo(map);
-  L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 0,
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
-  }).addTo(map);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
   setState({map: map})
 }
 
